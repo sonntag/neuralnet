5a3580a206431837d3c4fcd33269388b88784a34
Justin Sonntag <justinsonntag@r225128.res.Lehigh.EDU>
2009-02-24 16:10:00 -0500

    began writing init method for node

--- node.py
+++ node.py
@@ -1,9 +1,14 @@
 # File: node.py
 # Copyright 2009 Justin Sonntag. All rights reserved.
 
+# Types of neurons:
+#	pos: positive neuron with activation between 0 and 1
+#	neg: negative neuron with activation between 0 and -1
+#	bias: bias neuron with activation equal to 1
+
 class node (object):
 
-	def __init__(self):
-		pass
-
-
+	def __init__(self, a = 0.0, l = 0.0, h = 1.0):
+		self.activation = a
+		self.low = l
+		self.high = h
