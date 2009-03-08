3f16140a01324430066d911df2160c344a4a7f23
Justin Sonntag <justinsonntag@lappy.local>
2009-03-08 18:37:30 -0400

    created file weightmatrix.py

---
+++ edge/weightmatrix.py
@@ -0,0 +1,11 @@
+# File: weightmatrix.py
+# Copyright 2009 Justin Sonntag. All rights reserved.
+
+import autopath
+
+import numpy as np
+from neuralnet.edge.edge import edge
+
+class weightmatrix (object):
+	
+	def __init__(self, i, j, 
